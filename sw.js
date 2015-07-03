2a355dac0c425e78c8431c0697617118f5a8ca4c
辛忠柱 <xzzw9987@gmail.com>
2015-07-03 14:31:27 +0800

    Update Code

--- sw.js
+++ sw.js
@@ -17,5 +17,5 @@ self.addEventListener('install', function (event) {
 self.addEventListener('fetch', function (event) {
     console.log('fetch');
     console.log(event.request.url);
-    event.respondWith(caches.match(event.request));
+    event.respondWith(caches.match(event.request).then(function(data){console.log(data)}));
 });
