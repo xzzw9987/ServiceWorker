5b3614623b51b1d92e8fba82c2d5893d35467219
辛忠柱 <xzzw9987@gmail.com>
2015-07-03 14:08:06 +0800

    Update Code

--- sw.js
+++ sw.js
@@ -1,13 +1,22 @@
 /**
  * Created by baidu on 15/7/3.
  */
-/*
 self.addEventListener('install', function (event) {
-
+    console.log('install');
+    var cachesUrl = [
+        './',
+        './index.html',
+        './src',
+        './src/main.js'
+    ];
     event.waitUntil(function () {
         caches.open('v1')
             .then(function (cache) {
-
-            })
-    })
-});*/
+                cache.addAll(cachesUrl);
+            });
+    });
+});
+self.addEventListener('fetch', function (event) {
+    console.log('fetch');
+    event.respondWith(caches.match(event.request));
+});
