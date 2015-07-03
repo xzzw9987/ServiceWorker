316d4ac90bc79ffe7e6c8c70c37a9575db05070f
辛忠柱 <xzzw9987@gmail.com>
2015-07-03 13:42:28 +0800

    Update Code

--- src/main.js
+++ src/main.js
@@ -5,5 +5,8 @@ if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('./sw.js', {scope: './'})
         .then(function (a) {
             console.log(a);
+        })
+        .cathc(function (err) {
+            console.log(err);
         });
 }
