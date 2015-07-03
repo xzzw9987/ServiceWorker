a5c7b06e1dad5ff0c0ed78be79364593f0746b48
辛忠柱 <xzzw9987@gmail.com>
2015-07-03 13:49:40 +0800

    Update Code

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@
  * Created by baidu on 15/7/3.
  */
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('https://xzzw9987.github.io/ServiceWorker/src/sw.js', {scope: './'})
+    navigator.serviceWorker.register('sw.js', {scope: './'})
         .then(function (a) {
             console.log(a);
         })
